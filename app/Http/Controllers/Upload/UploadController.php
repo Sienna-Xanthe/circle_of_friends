589e66e4afab88f5d390489cc40438831a147f67
Sienna-Xanthe <3043015301@qq.com>
2021-11-19 21:06:00 +0800

    pxy11/19-2

--- app/Http/Controllers/Upload/UploadController.php
+++ app/Http/Controllers/Upload/UploadController.php
@@ -19,7 +19,10 @@ class UploadController extends Controller
         //获取上传的文件
         $file = $request->file('file');
         //判断文件类型，如果不是图片会返回false
-        $a=exif_imagetype($file);
+        //$a=exif_imagetype($file);
+        $a = getimagesize($file);
+//        var_dump($b);
+//        echo $a[2];
         //获取上传图片的临时地址
         $tmppath = $file->getRealPath();
         //生成文件名
@@ -27,7 +30,7 @@ class UploadController extends Controller
         //拼接上传的文件夹路径(按照日期格式1810/17/xxxx.jpg)
         $pathName = date('Y-m/d').'/'.$fileName;
         //上传图片到阿里云OSS
-        if($a==false){
+        if($a[2]==false){
             $mime = $file->getMimeType();
             //判断上传文件是不是视频
             if ($mime == "video/x-flv" || $mime == "video/mp4" || $mime == "application/x-mpegURL" || $mime == "video/MP2T" || $mime == "video/3gpp" || $mime == "video/quicktime" || $mime == "video/x-msvideo" || $mime == "video/x-ms-wmv"){
